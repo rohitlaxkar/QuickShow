786e9cbf898f2b74c063d1b8769ad3bd71b309c2
Rohit Laxkar <rohitlaxkar450@gmail.com>
2026-01-22 10:59:01 +0530

    cp,m

--- client/src/components/TrailersSection.jsx
+++ client/src/components/TrailersSection.jsx
@@ -11,7 +11,7 @@ const TrailersSection = () => {
   return (
     <div className='px-6 md:px-16 lg:px-24 xl:px-44 py-20 overflow-hidden'>
         <p className='text-gray-300 font-medium text-lg max-w-[960px]
-        mx-auto'>Trailers</p>
+        mx-auto'>Trailersers</p>
       
       <div className='relative mt-6'>
         <BlurCircle top='-100px' right='-100px'/>
@@ -32,7 +32,7 @@ const TrailersSection = () => {
           -translate-y-1/2'/> 
         </div>
 
-        
+
       ))}
       </div>
     </div>
